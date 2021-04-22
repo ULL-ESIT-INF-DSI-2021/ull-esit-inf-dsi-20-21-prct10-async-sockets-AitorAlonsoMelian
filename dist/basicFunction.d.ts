e19d5e2b0412f47d0cb8a87ba2f6830ec264a10c
Aitor Alonso <alu0101122496@ull.edu.es>
2021-04-22 10:49:50 +0100

    Basic Template

---
+++ dist/basicFunction.d.ts
@@ -0,0 +1,1 @@
+export declare function add(valor1: number, valor2: number): number;
---
+++ src/basicFunction.ts
@@ -0,0 +1,9 @@
+/**
+ * Método que suma 2 numeros.
+ * @param valor1 Primer número
+ * @param valor2 Segundo número
+ * @returns Resultado de la suma 
+ */
+export function add(valor1: number, valor2:number): number {
+    return valor1 + valor2;
+  }
